fb0e6e25ffc86ec23534427e2bed7abf01ac14f1
Shivamj4112 <shivamj4112@gmail.com>
2024-03-13 09:33:04 +0530

    Commit

--- app/src/main/java/com/example/stepstracker/view/activity/payments/ChoosePlansActivity.kt
+++ app/src/main/java/com/example/stepstracker/view/activity/payments/ChoosePlansActivity.kt
@@ -6,12 +6,16 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.stepstracker.R
+import com.example.stepstracker.databinding.ActivityChoosePlansBinding
 import com.example.stepstracker.util.statusBarColorGrey
 
 class ChoosePlansActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityChoosePlansBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_choose_plans)
+        binding = ActivityChoosePlansBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         statusBarColorGrey()
 
 
